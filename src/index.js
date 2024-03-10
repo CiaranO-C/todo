5896bef69e826613afc5b589a29248e586595096
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-03-10 23:18:29 +0000

    allow users to edit task category and name from displays.
    event listeners applied as elements are created
    and appended to doc

--- src/index.js
+++ src/index.js
@@ -158,13 +158,13 @@ function createTodo(title, description, dueDate, priority) {
 
 function generateExample() {
     //This will be triggered by using GUI
-    addTaskToArray(createTask('Train Paw', 'Dog Training'));
+    addTaskToArray(createTask('Train Paw', 'pet_supplies'));
     attachToTask(allTasks[0], createTodo('Step One', 'Mark and treat when dog moves paws', 'today', '1'));
     attachToTask(allTasks[0], createTodo('Step two', 'Hold hand by paw, whenever dog brushes hand mark and treat', 'tomorrow', '2'));
 
 
     //this is a simplified breakdown of what the above achieves using the utility functions
-    const taskTwo = new Task('Train Sit', "Work");
+    const taskTwo = new Task('Train Sit', "sound_detection_dog_barking");
     const todoThree = new Todo('Step One', 'Mark and treat when dog moves paws', 'today', '2');
     const todoFour = new Todo('Step two', 'treat when dog sits', 'tomorrow', '1');
     attachToTask(taskTwo, todoThree);
@@ -189,9 +189,9 @@ function filterAllTasks(category) {
 //then simply combine parentTask.getTodos() and index attribute and splice(index, 1); 
 //once removed ensure to re render display and re-assign attributes to new index values
 function getParentTask(parentTitle) {
-   const parentTask = allTasks.find((task) => task.getTitle() === parentTitle);
+    const parentTask = allTasks.find((task) => task.getTitle() === parentTitle);
 
-   return parentTask;
+    return parentTask;
 }
 
 
@@ -231,16 +231,6 @@ function thisWeeksTodos() {
     return filteredTodos;
 }
 
-
-//checkStorage();
-generateExample();
-console.log(allTasks);
-console.log(collateTodos(allTasks));
-console.log(filterTodos("complete", true));
-console.log(filterTodos("complete", false));
-
-console.log(filterTodos("priority", "1"));
-console.log(filterTodos("priority", "2"));
 //some sort of object which stores things like
 //currently selected task
 //is filtered true/false, maybe its a allTasks obj
@@ -254,6 +244,148 @@ allTasks = {
 */
 
 /////////////////////////DOM STUFF//////////////////////////////////////
-const allTasksPage = (function () {
+function allTasksPage() {
+
+    const mainContent = document.querySelector('.content');
+
+    const pageTitle = document.createElement('h1');
+    pageTitle.textContent = 'Your Tasks';
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('tasks-container');
+
+    mainContent.append(pageTitle, taskContainer);
+
+    if (allTasks.length) {
+        for (let i = 0; i < allTasks.length; i++) {
+            const task = allTasks[i];
+
+            const taskDiv = document.createElement('div');
+
+            const header = document.createElement('header');
+            header.classList.add('task-header');
+
+            const categoryIcon = document.createElement('span');
+            categoryIcon.classList.add('material-symbols-outlined');
+            categoryIcon.textContent = task.getCategory();
+
+            const taskTitle = document.createElement("input");
+            taskTitle.type = "text";
+            taskTitle.readOnly = true;
+            taskTitle.value = task.getTitle();
+
+            header.append(categoryIcon, taskTitle);
+
+            const section = document.createElement('section');
+
+            const todoCounter = document.createElement('div');
+            todoCounter.classList.add('todos-amount');
+
+            const todosText = document.createElement('span');
+            todosText.textContent = 'Todos';
+
+            const line = document.createElement('div');
+            line.classList.add('line');
+
+            const todoNumber = document.createElement('span');
+            todoNumber.textContent = task.getTodos().length;
+
+            todoCounter.append(todosText, line, todoNumber);
+
+            const buttonContainer = document.createElement('div');
+            buttonContainer.classList.add('task-button-container');
+
+            const editButton = document.createElement('button');
+            const deleteButton = document.createElement('button');
+
+            editButton.setAttribute('data-btnType', 'edit'); //check if needed
+            deleteButton.setAttribute('data-btnType', 'delete'); //check if needed
+
+            editButton.classList.add('material-symbols-outlined');
+            deleteButton.classList.add('material-symbols-outlined');
+
+            editButton.textContent = 'edit';
+            deleteButton.textContent = 'delete';
+
+            buttonContainer.append(editButton, deleteButton);
+
+            section.append(todoCounter, buttonContainer);
+            taskDiv.append(header, section);
+
+            taskDiv.setAttribute('data-task-index', `${i}`);
+            taskListeners(taskDiv, task);
+            taskContainer.appendChild(taskDiv);
+        };
+    } else {
+        const addTask = document.createElement('div');
+        addTask.id = 'addTask';
+
+        const addIcon = document.createElement('span');
+        addIcon.classList.add('material-symbols-outlined');
+        addIcon.textContent = 'add_circle';
+
+        addTask.appendChild(addIcon);
+
+        taskContainer.appendChild(addTask);
+    };
+
+    function taskListeners(taskElement, taskObject) {
+        const [icon, titleInput] = taskElement.children[0].children;
+        const [editBtn, deleteBtn] = taskElement.children[1].children[1].children;
+
+        editBtn.addEventListener('click', () => {
+            const status = editBtn.textContent;
+            console.log(status);
+            if (status === 'edit') {
+                icon.classList.toggle('edit');
+                titleInput.classList.toggle('edit');
+                titleInput.readOnly = false;
+                editBtn.textContent = 'done';
+                icon.addEventListener('click', modalListeners);
+            } else {
+                icon.classList.toggle('edit');
+                titleInput.classList.toggle('edit');
+                titleInput.readOnly = true;
+                editBtn.textContent = 'edit';
+                icon.removeEventListener('click', modalListeners);
+                updateTaskObject();
+                saveToStorage();
+            };
+        });
+
+        function modalListeners(event) {
+            const modal = document.querySelector('#myModal');
+            const closeBtn = document.querySelector('#closeModal');
+            const confirmBtn = document.querySelector('#modalConfirm');
+
+            modal.classList.toggle('hidden'); //displays the modal
+
+            closeBtn.addEventListener('click', closeModal);
+            confirmBtn.addEventListener('click', changeIcon);
+
+            function closeModal() {
+                modal.classList.toggle('hidden');
+                closeBtn.removeEventListener('click', closeModal);
+                confirmBtn.removeEventListener('click', changeIcon);
+            };
+
+            function changeIcon() {
+                const selectedIcon = document.querySelector('input[name="category"]:checked').id;
+                icon.textContent = selectedIcon;
+                closeModal();
+            };
+        };
+
+        function updateTaskObject() {
+            console.log(taskObject);
+            taskObject.setCategory(icon.textContent);
+    
+            taskObject.setTitle(titleInput.value);
+        };
+    };
+};
+
+checkStorage();
+//generateExample();
 console.log(allTasks);
-})();
+allTasksPage();
